f69412895845d7f1b6a9397651e2bbe9d27f7c93
bgyuri76 <bgyuridev@gmail.com>
2016-07-27 15:59:17 +0300

    full app, not working

--- RealmDatabase/app/src/main/java/com/example/gyuri/realmdatabase/adapters/RealmBooksAdapter.java
+++ RealmDatabase/app/src/main/java/com/example/gyuri/realmdatabase/adapters/RealmBooksAdapter.java
@@ -3,5 +3,15 @@ package com.example.gyuri.realmdatabase.adapters;
 /**
  * Created by gyuri on 7/27/2016.
  */
-public class RealmBooksAdapter {
+        import android.content.Context;
+
+        import com.example.gyuri.realmdatabase.model.Book;
+        import io.realm.RealmResults;
+
+public class RealmBooksAdapter extends RealmModelAdapter<Book> {
+
+    public RealmBooksAdapter(Context context, RealmResults<Book> realmResults, boolean automaticUpdate) {
+
+        super(context, realmResults, automaticUpdate);
+    }
 }
